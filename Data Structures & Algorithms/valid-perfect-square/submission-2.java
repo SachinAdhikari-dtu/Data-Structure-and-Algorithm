9fa60a364cf42058fc2be68ee16d1504bd8c91f7
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-07-28 19:43:25 +0530

    Add: valid-perfect-square - submission-2

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long s=1;
+        long e=num;
+        while(s<=e){
+            long m = s +(e-s)/2;
+            if(m*m==num)return true;
+            else if(m*m > num){
+                e=m-1;
+            }else{
+                s=m+1;
+            }
+        }
+        return false;
+    }
+}
