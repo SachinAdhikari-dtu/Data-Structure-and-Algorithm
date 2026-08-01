6a685db5cccdf0d3c7ee0d32ea0930e3f94ebdf5
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-08-02 02:46:48 +0530

    Add: power-of-two - submission-3

---
+++ Data Structures & Algorithms/power-of-two/submission-3.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)return false;
+        if(n==Integer.MIN_VALUE)return false;
+        if(n==1||n==2)return true;
+        return (n&(n-1))==0;
+    }
+}
