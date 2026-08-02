77cdc02334e722cdba0c3feb40144d294126e175
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-08-02 22:37:20 +0530

    Add: n-ary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/n-ary-tree-postorder-traversal/submission-0.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+
+    public static void help(List<Integer> l , Node root){
+        if(root==null)return;
+        for(Node x :root.children){
+            help(l,x);
+        }
+        l.add(root.val);
+    }
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> l = new ArrayList<>();
+        help(l,root);
+        return l;
+    }
+
+
+    
+
+
+}
